0811416f65f4fd4592771cac1d17a7fbf192ca7b
moshaosama <mohamedOSFekry@gmail.com>
2025-08-13 16:08:11 +0300

    feat: add constants folder

---
+++ src/constants/SLiderData.tsx
@@ -0,0 +1,21 @@
+interface SliderDataType {
+  title: string;
+}
+
+export const SliderData: SliderDataType[] = [
+  {
+    title: "Collections",
+  },
+
+  {
+    title: "Environments",
+  },
+
+  {
+    title: "Flows",
+  },
+
+  {
+    title: "History",
+  },
+];
