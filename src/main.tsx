0043cb7b47ecbac72867160578e3ddcde1e86212
moshaosama <mohamedOSFekry@gmail.com>
2025-08-13 16:08:04 +0300

    chore: update main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import OpenSliderProvider from "./context/OpenSliderContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <OpenSliderProvider>
+      <App />
+    </OpenSliderProvider>
   </StrictMode>
 );
