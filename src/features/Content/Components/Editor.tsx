33bdef7af0c7c97654fc5441ddd7e586b2b93a70
moshaosama <mohamedOSFekry@gmail.com>
2025-08-14 23:44:52 +0300

    feat: update Editor component

--- src/features/Content/Components/Editor.tsx
+++ src/features/Content/Components/Editor.tsx
@@ -1,7 +1,10 @@
+import { useOpenSliderContext } from "../../../context/OpenSliderContext";
+import { cn } from "../../../lib/utils";
 import { useState } from "react";
 import AceEditor from "react-ace";
 const Editor = () => {
   const [code, setCode] = useState("");
+  const { isOpenSlider } = useOpenSliderContext();
   return (
     <>
       <AceEditor
@@ -10,7 +13,7 @@ const Editor = () => {
         name="my_editor"
         onChange={(newValue) => setCode(newValue)}
         fontSize={14}
-        width="100%"
+        width={cn(isOpenSlider ? "100%" : "100%")}
         height="350px"
         value={code}
         setOptions={{
