9bd112a5fce27bb9bdc8e8ca927d05e7816a7d3c
moshaosama <mohamedOSFekry@gmail.com>
2025-08-15 00:41:03 +0300

    feat: update SendRequestService implementation

--- src/api/SendRequest/SendRequestService.ts
+++ src/api/SendRequest/SendRequestService.ts
@@ -3,10 +3,20 @@ import axios from "axios";
 
 class SendRequestService extends ParentService {
   createData(): void {}
-  async getData(url?: string, thunkApi?: any) {
+  async getData(
+    data: { url: string; method: string; body?: any },
+    thunkApi?: any
+  ) {
     try {
-      const response = await axios.get(url as string);
-      return response.data;
+      const response = await axios({
+        method: data.method,
+        url: data.url,
+        data: data.body,
+      });
+      return {
+        data: response.data,
+        statusbar: response.status,
+      };
     } catch (err) {
       return thunkApi.rejectWithValue(err);
     }
