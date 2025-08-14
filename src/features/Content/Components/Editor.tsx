b0a3deb2263e60cc68a1cbce15b3ccf1d497d824
moshaosama <mohamedOSFekry@gmail.com>
2025-08-15 00:41:03 +0300

    feat: enhance Editor component

--- src/features/Content/Components/Editor.tsx
+++ src/features/Content/Components/Editor.tsx
@@ -1,26 +1,28 @@
+import { useGetDataContext } from "../../../context/GetDataContext";
 import { useOpenSliderContext } from "../../../context/OpenSliderContext";
 import { cn } from "../../../lib/utils";
-import { useState } from "react";
 import AceEditor from "react-ace";
 const Editor = () => {
-  const [code, setCode] = useState("");
   const { isOpenSlider } = useOpenSliderContext();
+  const { code, setCode } = useGetDataContext();
   return (
     <>
-      <AceEditor
-        mode="json"
-        theme="github"
-        name="my_editor"
-        onChange={(newValue) => setCode(newValue)}
-        fontSize={14}
-        width={cn(isOpenSlider ? "100%" : "100%")}
-        height="350px"
-        value={code}
-        setOptions={{
-          showLineNumbers: true,
-          tabSize: 2,
-        }}
-      />
+      <div>
+        <AceEditor
+          mode="json"
+          theme="github"
+          name="my_editor"
+          onChange={(newValue) => setCode(newValue)}
+          fontSize={14}
+          width={cn(isOpenSlider ? "100%" : "100%")}
+          height="350px"
+          value={code}
+          setOptions={{
+            showLineNumbers: true,
+            tabSize: 2,
+          }}
+        />
+      </div>
     </>
   );
 };
