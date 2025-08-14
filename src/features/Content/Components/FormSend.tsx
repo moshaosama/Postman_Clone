9f0b8a6c11e8c5f79df18d35c018dfcf3feaf3f9
moshaosama <mohamedOSFekry@gmail.com>
2025-08-14 18:28:50 +0300

    feat: update FormSend component

--- src/features/Content/Components/FormSend.tsx
+++ src/features/Content/Components/FormSend.tsx
@@ -1,16 +1,15 @@
 import useSendRequest from "../Hooks/useSendRequest";
+import RenderData from "./RenderData";
 
 const FormSend = () => {
-  const { Data, handleSendrequest } = useSendRequest();
-
-  const handleClick = async (e: any) => {
-    e.preventDefault();
-    await handleSendrequest();
-  };
+  const { handleSendrequest, register, handleSubmit } = useSendRequest();
 
   return (
     <>
-      <form className="py-3 px-8 flex">
+      <form
+        onSubmit={handleSubmit(handleSendrequest)}
+        className="py-3 px-8 flex"
+      >
         <select className="border-t-[#cfcfcf] focus:outline-0 w-30 border-l-[#cfcfcf] border-b-[#cfcfcf]  border-t-2 border-l-2 border-b-2 rounded-l-sm  rounded-tl-sm rounded-bl-sm">
           <option
             className="text-green-500 font-semibold"
@@ -37,24 +36,17 @@ const FormSend = () => {
         </select>
         <input
           type="text"
+          {...register("url", { required: true })}
           className="border-r-[#cfcfcf] focus:outline-0 border-t-[#cfcfcf] border-b-[#cfcfcf] border-b-2 border-t-2 border-r-2 w-full p-2 rounded-r-sm rounded-tr-sm rounded-br-sm"
         />
 
         <p>
-          <button
-            onClick={handleClick}
-            className="bg-blue-500 text-white font-bold w-32 h-10 mx-2 rounded-sm hover:bg-blue-600 transition-all duration-300 cursor-pointer"
-          >
+          <button className="bg-blue-500 text-white font-bold w-32 h-10 mx-2 rounded-sm hover:bg-blue-600 transition-all duration-300 cursor-pointer">
             Send
           </button>
         </p>
       </form>
-
-      <div className="mt-4">
-        <pre className="bg-gray-100 p-4 rounded">
-          {Data ? JSON.stringify(Data, null, 2) : null}
-        </pre>
-      </div>
+      <RenderData />
     </>
   );
 };
