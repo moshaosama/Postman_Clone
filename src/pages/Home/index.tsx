4c2bb7695adf88b2c7a9f8fcdb961c201f9d7523
moshaosama <mohamedOSFekry@gmail.com>
2025-08-16 20:58:43 +0300

    Update Home/index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,12 +3,14 @@ import FormSend from "../../features/Content/Components/FormSend";
 import { HeaderTabs } from "../../features/Tabs/components/HeaderTabs";
 import RenderData from "../../features/Content/Components/RenderData";
 import Editor from "../../features/Content/Components/Editor";
+import HeaderContent from "../../features/Content/Components/HeaderContent";
 
 const Home = () => {
   return (
     <>
       <div className="w-full">
         <HeaderTabs />
+        <HeaderContent />
         <FormSend />
         <SelectorRequest />
         <Editor />
