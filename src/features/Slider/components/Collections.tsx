334681e047050f4085d448f5ef1e1bf910893b48
moshaosama <mohamedOSFekry@gmail.com>
2025-08-16 20:58:42 +0300

    Update Collections.tsx

--- src/features/Slider/components/Collections.tsx
+++ src/features/Slider/components/Collections.tsx
@@ -1,8 +1,12 @@
+import useGetHistory from "../../../features/History/Hooks/useGetHistory";
 import { useOpenSliderContext } from "../../../context/OpenSliderContext";
 import { Search } from "lucide-react";
+import { cn } from "../../../lib/utils";
+import { MethodFactory } from "../../../lib/MethodFactory";
 
 const Collections = () => {
   const { isOpenSlider } = useOpenSliderContext();
+  const { History } = useGetHistory();
   return (
     <>
       {isOpenSlider && (
@@ -19,6 +23,26 @@ const Collections = () => {
               placeholder="Search collections"
             />
           </form>
+
+          <div className="my-4 flex flex-col gap-2">
+            {Array.isArray(History) &&
+              History.map((history) => (
+                <div
+                  key={history.id}
+                  className="text-[15px] flex items-center gap-1 p-1 cursor-pointer hover:bg-[#cccccc] rounded-lg"
+                >
+                  <h1
+                    className={cn(
+                      MethodFactory(history.method),
+                      "font-semibold"
+                    )}
+                  >
+                    {history.method}
+                  </h1>
+                  <p className="truncate w-52">{history.url}</p>
+                </div>
+              ))}
+          </div>
         </div>
       )}
     </>
