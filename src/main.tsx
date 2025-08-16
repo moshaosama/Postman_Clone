f82f9789b04f46e34bdbc57efa23de88b13c94e4
moshaosama <mohamedOSFekry@gmail.com>
2025-08-16 20:58:43 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
 import OpenSliderProvider from "./context/OpenSliderContext.tsx";
 import GetDataProvider from "./context/GetDataContext.tsx";
+import { store } from "./store/Store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <OpenSliderProvider>
-      <GetDataProvider>
-        <App />
-      </GetDataProvider>
+      <Provider store={store}>
+        <GetDataProvider>
+          <App />
+        </GetDataProvider>
+      </Provider>
     </OpenSliderProvider>
   </StrictMode>
 );
