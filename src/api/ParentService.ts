179f62cdef23a2e84ecfbc14baa3b9289a5f0e3f
moshaosama <mohamedOSFekry@gmail.com>
2025-08-16 20:58:42 +0300

    Update ParentService.ts

--- src/api/ParentService.ts
+++ src/api/ParentService.ts
@@ -1,4 +1,5 @@
 export abstract class ParentService {
+  url = "http://localhost:3000/api/";
   abstract getData(_?: any, thunkApi?: any): void;
   abstract createData(data?: any, thunkApi?: any): void;
 }
