681fcfd2af8b3a214a7b92e27af72ca1629e27b2
moshaosama <mohamedOSFekry@gmail.com>
2025-08-17 03:34:57 +0300

    Update Store.ts

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import historyReducer from "../features/History/Reducers/HistoryReducer";
+import historyByIdReducer from "../features/History/Reducers/HistoryByIdReducer";
 export const store = configureStore({
   reducer: {
     history: historyReducer,
+    historyById: historyByIdReducer,
   },
 });
 
