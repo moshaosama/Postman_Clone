78c92f3f7cd6f875ad3bfe343ac6c3c7fe9e36cd
moshaosama <mohamedOSFekry@gmail.com>
2025-08-17 03:34:56 +0300

    Update HistoryService.ts

--- src/api/History/HistoryService.ts
+++ src/api/History/HistoryService.ts
@@ -4,10 +4,7 @@ import { ParentService } from "../ParentService";
 class HistoryService extends ParentService {
   async createData(data: { method: string; url: string }, thunkApi?: any) {
     try {
-      const response = await axios.post(
-        `http://localhost:3000/api/history`,
-        data
-      );
+      const response = await axios.post(`${this.url}history`, data);
       return response.data;
     } catch (err) {
       return thunkApi.rejectWithValue(err);
@@ -16,6 +13,15 @@ class HistoryService extends ParentService {
   async getData(_?: any, thunkApi?: any) {
     try {
       const response = await axios.get(`${this.url}history`);
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
+
+  async GetDataByid(id: number, thunkApi?: any) {
+    try {
+      const response = await axios.get(`${this.url}history/${id}`);
       return response.data;
     } catch (err) {
       return thunkApi.rejectWithValue(err);
