fc45dd3491ff005310f43fdb838d37e55b6173b9
moshaosama <mohamedOSFekry@gmail.com>
2025-08-17 15:25:22 +0300

    Update FormSend.tsx

--- src/features/Content/Components/FormSend.tsx
+++ src/features/Content/Components/FormSend.tsx
@@ -17,11 +17,13 @@ const FormSend = () => {
       >
         <select
           {...register("method", { required: true })}
+          id="selectInput"
           className="border-t-[#cfcfcf] focus:outline-0 w-30 border-l-[#cfcfcf] border-b-[#cfcfcf]  border-t-2 border-l-2 border-b-2 rounded-l-sm  rounded-tl-sm rounded-bl-sm"
         >
           <option
             className="text-green-500 font-semibold"
             value="Get"
+            id="getMethod"
             defaultChecked
           >
             Get
@@ -45,12 +47,16 @@ const FormSend = () => {
         <input
           type="text"
           {...register("url", { required: true })}
+          id="searchInput"
           defaultValue={HistoryById?.url || ""}
           className="border-r-[#cfcfcf] focus:outline-0 border-t-[#cfcfcf] border-b-[#cfcfcf] border-b-2 border-t-2 border-r-2 w-full p-2 rounded-r-sm rounded-tr-sm rounded-br-sm"
         />
 
         <p>
-          <button className="bg-blue-500 text-white font-bold w-32 h-10 mx-2 rounded-sm hover:bg-blue-600 transition-all duration-300 cursor-pointer">
+          <button
+            id="sendBtn"
+            className="bg-blue-500 text-white font-bold w-32 h-10 mx-2 rounded-sm hover:bg-blue-600 transition-all duration-300 cursor-pointer"
+          >
             Send
           </button>
         </p>
