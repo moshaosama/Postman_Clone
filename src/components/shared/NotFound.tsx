332360e6f99a33b9b7a9b81c08fa34088953bdaf
moshaosama <mohamedOSFekry@gmail.com>
2025-08-17 03:35:01 +0300

    Add shared components

---
+++ src/components/shared/NotFound.tsx
@@ -0,0 +1,18 @@
+const NotFound = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-[15pc]">
+        <img
+          src="192141479_3992880990790250_33645994725439873_n-removebg-preview.png"
+          alt="NotFound.png"
+          className="w-60"
+        />
+        <h1 className="text-[gray] text-sm">
+          Enter the URL and click Send to get a response
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
