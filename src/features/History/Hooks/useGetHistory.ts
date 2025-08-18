0ff1c76439842bfbf63eff4bdabb093e36a85d4d
moshaosama <mohamedOSFekry@gmail.com>
2025-08-18 19:06:26 +0300

    Update useGetHistory hook

--- src/features/History/Hooks/useGetHistory.ts
+++ src/features/History/Hooks/useGetHistory.ts
@@ -1,16 +1,14 @@
 import { RootState, AppDispatch } from "@/store/Store";
-import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchGetHistory } from "../Actions/HistoryActions";
 
 const useGetHistory = () => {
   const { data: History } = useSelector((state: RootState) => state.history);
   const dispatch = useDispatch<AppDispatch>();
-  useEffect(() => {
-    dispatch(fetchGetHistory());
-  }, []);
-
-  return { History };
+  const handleGetHistory = (collection_id: number) => {
+    dispatch(fetchGetHistory(collection_id));
+  };
+  return { History, handleGetHistory };
 };
 
 export default useGetHistory;
