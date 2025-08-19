e766b966b55725f264f1f9cd05c180caa070c07d
moshaosama <mohamedOSFekry@gmail.com>
2025-08-19 20:09:29 +0300

    Update DefaultTabs.tsx

--- src/features/Tabs/components/DefaultTabs.tsx
+++ src/features/Tabs/components/DefaultTabs.tsx
@@ -1,12 +1,19 @@
-// import { X } from "lucide-react";
+import usegetTabs from "../Hooks/usegetTabs";
 
 const DefaultTabs = () => {
+  const { Tabs } = usegetTabs();
+
   return (
     <>
-      <div className="px-5 flex items-center group text-sm gap-2">
-        <h1 className="text-green-500">get</h1>
-        <p>Untitled Request</p>
-        {/* <X size={15} className="group-hover:block hidden" /> */}
+      <div className="flex items-center justify-center">
+        {Array.isArray(Tabs) &&
+          Tabs.map((tabs) => (
+            <div className="px-2 hover:bg-[#e2e2e2]  flex items-center group text-sm gap-2">
+              <h1 className="text-green-500">{tabs.method}</h1>
+              <p>{tabs.url}</p>
+              <hr className="h-6 w-[1px] bg-[#d4d4d4]" />
+            </div>
+          ))}
       </div>
     </>
   );
