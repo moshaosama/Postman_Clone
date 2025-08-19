70ae92e0aa1548b1613b31075a1ce572c072cffd
moshaosama <mohamedOSFekry@gmail.com>
2025-08-19 20:32:45 +0300

    Update HeaderTabs.tsx

--- src/features/Tabs/components/HeaderTabs.tsx
+++ src/features/Tabs/components/HeaderTabs.tsx
@@ -1,13 +1,16 @@
 import { Plus } from "lucide-react";
 import DefaultTabs from "./DefaultTabs";
+import useCreateTabs from "../Hooks/useCreateTabs";
 
 export const HeaderTabs = () => {
+  const { handleCreateTab } = useCreateTabs();
   return (
     <>
       <div className="flex items-center  border-b-2 p-2 border-b-[#ececec] ">
         <DefaultTabs />
         <div>
           <Plus
+            onClick={handleCreateTab}
             color="gray"
             size={30}
             className="hover:bg-[#d6d6d6] p-1 rounded-lg cursor-pointer"
