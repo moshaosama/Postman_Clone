3448b2122d1205866e4f397311003385db20b784
moshaosama <mohamedOSFekry@gmail.com>
2025-08-20 16:35:42 +0300

    Update TabService.ts

--- src/api/Tabs/TabService.ts
+++ src/api/Tabs/TabService.ts
@@ -1,23 +1,34 @@
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
 import { ParentService } from "../ParentService";
 
 export class TabService extends ParentService {
-  async createData(_?: any, thunkApi?: any) {
+  async createData(_?: unknown, thunkApi?: any) {
     try {
-      const response = await axios.post(`${this.url}tabs`);
+      const response: AxiosResponse = await axios.post(`${this.url}tabs`);
       return response.data;
     } catch (err) {
       return thunkApi.rejectWithValue(err);
     }
   }
-  async getData(_?: any, thunkApi?: any) {
+  async getData(_?: unknown, thunkApi?: any) {
     try {
-      const response = await axios.get(`${this.url}tabs`);
+      const response: AxiosResponse = await axios.get(`${this.url}tabs`);
       return response.data;
     } catch (err) {
       return thunkApi.rejectWithValue(err);
     }
   }
+
+  async DeleteData(id:number, thunkApi?: any) {
+      try {
+          const response: AxiosResponse =  await axios.delete(`http://localhost:3000/api/tabs/${id}`);
+          return  response.data;
+      }
+      catch (err) {
+          return thunkApi.rejectWithValue(err);
+      }
+  }
+  
 }
 
 export const tabsService = new TabService();
