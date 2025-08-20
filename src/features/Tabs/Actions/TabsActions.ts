638462766615f4283b6255e0fce9185dfb5022cb
moshaosama <mohamedOSFekry@gmail.com>
2025-08-20 20:10:09 +0300

    Update TabsActions.ts

--- src/features/Tabs/Actions/TabsActions.ts
+++ src/features/Tabs/Actions/TabsActions.ts
@@ -22,3 +22,11 @@ export const fetchDeleteTab = createAsyncThunk("Tabs/fetchDeleteTab", async (id:
 export const fetchGetTabByid = createAsyncThunk("TabByid/fetchGetTabByid", async (id: number) => {
     return await tabsService.getTabById(id)
 })
+
+
+export const fetchUpdateTabByid = createAsyncThunk(
+    "TabByid/fetchUpdateTabByid",
+    async function ({ id, updateTabDTO }: { id: number; updateTabDTO: { url: string; method: string } }) {
+        return await tabsService.updateTabById({ id, updateTabDTO });
+    }
+);
