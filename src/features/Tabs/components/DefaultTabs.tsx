97f4bf642b12133dbbe0da3a21af28c58bf954ce
moshaosama <mohamedOSFekry@gmail.com>
2025-08-20 16:35:43 +0300

    Update DefaultTabs.tsx

--- src/features/Tabs/components/DefaultTabs.tsx
+++ src/features/Tabs/components/DefaultTabs.tsx
@@ -1,22 +1,24 @@
 import usegetTabs from "../Hooks/usegetTabs";
-
+import {X} from "lucide-react";
+import {useDeleteTabs} from "../Hooks/useDeleteTabs.ts";
 const DefaultTabs = () => {
   const { Tabs } = usegetTabs();
-
-  return (
+  const {handleDeleteTabs} = useDeleteTabs()
+    return (
     <>
       <div
-        className="grid"
+        className="grid gap-4"
         style={{ gridTemplateColumns: `repeat(${Tabs.length}, 1fr)` }}
       >
         {Array.isArray(Tabs) &&
           Tabs.map((tab) => (
             <div
               key={tab.id}
-              className="px-2 hover:bg-[#e2e2e2] py-1 cursor-pointer flex items-center gap-2 text-sm"
+              className="px-2 group hover:bg-[#e2e2e2] transition-all duration-500 py-1 cursor-pointer flex items-center gap-1 text-[14px]"
             >
               <h1 className="text-green-500">{tab.method}</h1>
               <p>{tab.url}</p>
+                <X onClick={() => handleDeleteTabs(tab.id)} size={25} className="mx-2 rounded-xl opacity-0 group-hover:opacity-100 hover:bg-[#ddd] p-1"/>
             </div>
           ))}
       </div>
